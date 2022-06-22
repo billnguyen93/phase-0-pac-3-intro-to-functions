6997d09dc3da608f0c2e479c790918cc8089ebd6
Bill Nguyen <billfl93@gmail.com>
2022-06-22 02:19:36 -0400

    commit tree 22397f1e9f20b93418a4f8e1033e43f882bc6263

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+
+  function sayHelloToGuadalupe() {
+      console.log("Hello, Guadalupe!");
+  }
+  sayHelloToGuadalupe();
+
+  function sayHelloToLiz() {
+      console.log("Hello, Liz!");
+  }
+  sayHelloToLiz();
+
+  function sayHelloToSamip() {
+      console.log("Hello, Samip!");
+
+  }
+  sayHelloToSamip();
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+
+  say("Julio", "hello");
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  function add(x, y) {
+      return x + y;
+  }
+  console.log(add(1, 2));
